8be7410052694238586b2a06e0ca6bd9c45c5d19
g-tanwar <tanwarg282005@gmail.com>
2025-09-17 18:09:13 +0530

    Lesson 2 (Ignition)

--- App.js
+++ App.js
@@ -1,26 +1,40 @@
-const heading = React.createElement("h1", {id:"heading",xyz:"abc"}, "Hello World by React");      //<!-- core react --> 
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// {id:"heading",xyz:"abc"} : this is nothing just attributes which are stored as Props in object 
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc", key: "1" },
+  "Hello World by React"
+); //<!-- core react -->
 
+// {id:"heading",xyz:"abc"} : this is nothing just attributes which are stored as Props in object
 
-console.log(heading) // nothing just an object of js is what we also call it as react element
+console.log(heading); // nothing just an object of js is what we also call it as react element
 
-const root = ReactDOM.createRoot(document.getElementById("root"));                 //<!-- react DOM work  ROOT: root is the place where all the react stuff works everything renders inside this root-->
+const root = ReactDOM.createRoot(document.getElementById("root")); //<!-- react DOM work  ROOT: root is the place where all the react stuff works everything renders inside this root-->
 
-root.render(heading) // render just converts the object into h1 tag and putting up it in DOM 
+root.render(heading); // render just converts the object into h1 tag and putting up it in DOM
 
+// how to do nesting in this
 
-// how to do nesting in this 
-
-{/* <div id = "parent">
+{
+  /* <div id = "parent">
     <div id = "child">
         <h1>I am h1 tag</h1>
         <h1>I am h2 tag</h1>
     </div>
-</div> */}
+</div> */
+}
 
-
-const parent = React.createElement("div",{id:"parent"}, React.createElement("div",{id:"child"}, [React.createElement("h1",{},"I am h1 tag"), React.createElement("h1",{},"I am h2 tag")])) // If we want siblings of a tag. 
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {key:"2"}, "I am h1 tag"),
+    React.createElement("h1", {key:"3"}, "I am h2 tag"),
+  ])
+); // If we want siblings of a tag.
 
 // what if something already exists in the place we are rendering --> It will replace the earlier content with the new one. the earlier content will be loaded but for a very short time and will be replaced
+
 
---
+++ README.md
@@ -0,0 +1,19 @@
+# Parcel [The Bundler]
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement - (THE fast change in the code reflecting on the website very quickly)
+- File Watching Algorithim - written in C++ 
+- Casching - Faster Builds 
+- Image Optimization - (very heavy workload for any site to load images on the site)
+
+- Minify  --> Production
+- Bundling --> Production
+- Compress --> Production
+- Consistent Hashing 
+- Code Splitting 
+- Differential Bundling - (to support older browsers)
+- Diagnostic
+- Error Handling 
+- Host your HTTPS
+- Tree Shaking - Will remove unuseful code 
+- Different dev and proud bundles  
